31bbad025019620edfec71529f7d244222fd6598
unknown <anupshekharsharma@gmail.com>
2023-08-09 16:48:33 +0530

    ext file execution

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+declare var epson: any; 
+
+declare var externalProperty: any;
+declare var externalFunction: any;
+declare var externalObject: any;
 
 @Component({
   selector: 'app-home',
@@ -8,5 +13,22 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   constructor() {}
+
+  printer(){
+    const printer = new epson.ePOSDevice();
+    console.log('printer=> ',printer) 
+    console.log('epson=> ',epson) 
+  }
+
+  externalProperty(){
+    console.log('externalProperty=> ',externalProperty) 
+  }
+  externalFunction(){
+    console.log('externalFunction=> ',externalFunction) 
+  }
+
+  externalObject(){
+    console.log('externalObject=> ',externalObject) 
+  }
 
 }
